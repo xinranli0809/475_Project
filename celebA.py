7cc51712cf5131c604b07eb31d6458941b9471e2
xinranli0809 <xinranli2021@u.northwestern.edu>
2020-12-04 03:56:31 -0500

    It just works!
    The celebA.py file now has a random test function. Please note that the trained model is too large (around 100MB), so I will probably just upload it to google drive.

--- celebA.py
+++ celebA.py
@@ -1,22 +1,21 @@
 import pandas as pd
 import numpy as np
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import cv2
 import PIL
 import tensorflow as tf
 from tensorflow import keras
+from tensorflow.keras import layers
 
-TRAINING_SAMPLES = 10
-VALIDATION_SAMPLES = 2000
-TEST_SAMPLES = 2000
-IMG_WIDTH = 178
-IMG_HEIGHT = 218
-BATCH_SIZE = 16
-#just one epoch to save computing time, for more accurate results increase this number
-NUM_EPOCHS = 1
+# comment these off if giving you error
+physicalDevices = tf.config.list_physical_devices('GPU')
+# print(physicalDevices)
+tf.config.experimental.set_memory_growth(physicalDevices[0],True)
 
+## To use more than one attributes, we will do the same number of 2 class classification over all attributes
 #what characteristic are we going to train and test for? (note if set to 'all' it'll use all of them)
-ATTR='Blond_Hair'
+ATTR='Male'
 
 # set up working directory
 currentWorkingDirectory = os.getcwd()
@@ -46,7 +45,7 @@ partitions.set_index('image_id', inplace=True)
 
 
 parAttr = partitions.join(attributes[ATTR], how='inner')
-print(parAttr.shape)
+# print(parAttr.shape)
 
 def load_reshape_img(fname):
     # Load image as PIL format
@@ -64,7 +63,9 @@ def generate_df(df,partition, attr, num_samples):
         1 -> validation
         2 -> test
     '''
-    
+    ## This part is to balance the sample size for overly common or very uncommon attributes.
+    ##  Considering adding back in the future 
+
     #The sample size is at most the number stated above, but at least the size of the smallest class of the dataframe. This results in some uncommon attributes (e.g. sideburns) having to train on very few samples.
     # min_class_size=min(len(df[(df['partition'] == partition) & (df[attr] == 0)]),len(df[(df['partition'] == partition) & (df[attr] == 1)]) )
     # sample_size=int(num_samples/2)
@@ -76,33 +77,104 @@ def generate_df(df,partition, attr, num_samples):
     #                   df[(df['partition'] == partition) 
     #                               & (df[attr] == 1)].sample(sample_siz)])
 
+
     # sample desired amount of images of input partition
     df_ = df[(df['partition'] == partition)].sample(num_samples)
-    print(df_)
-    # for Train and Validation
-    if partition != 2:
-        # load images into shape of [number of images, 1, 218, 178, 3]
-        x_ = np.array([load_reshape_img(imageDirectory + fname) for fname in df_.index])
-        # reshape into the input shape for the model
-        x_ = x_.reshape(x_.shape[0], 218, 178, 3)
-        y_ = keras.utils.to_categorical(df_[attr],2)
-    # for Test
-    else:
-        x_ = []
-        y_ = []
+    # print(df_)
 
-        for index, target in df_.iterrows():
-            im = cv2.imread(imageDirectory + index)
-            im = cv2.resize(cv2.cvtColor(im, cv2.COLOR_BGR2RGB), (IMG_WIDTH, IMG_HEIGHT)).astype(np.float32) / 255.0
-            im = np.expand_dims(im, axis =0)
-            x_.append(im)
-            y_.append(target[attr])
+    x_ = np.array([load_reshape_img(imageDirectory + fname) for fname in df_.index])
+    # reshape into shape of [# of samples, 218, 178, 3]
+    x_ = x_.reshape(x_.shape[0], 218, 178, 3)
+    # one-hot encode the y values
+    y_ = keras.utils.to_categorical(df_[attr],2)
 
-    return x_, y_
+    ## This part is commented out because I don't understand why training and test data are processed in
+    ## very different ways yet in the end has the same shape. Use the above method for both train and test for now 
+    
+    # # for Train and Validation
+    # if partition != 2:
+    #     # load images into shape of [number of images, 1, 218, 178, 3]
+    #     x_ = np.array([load_reshape_img(imageDirectory + fname) for fname in df_.index])
+    #     # reshape into the input shape for the model
+    #     x_ = x_.reshape(x_.shape[0], 218, 178, 3)
+    #     y_ = keras.utils.to_categorical(df_[attr],2)
+    # # for Test
+    # else:
+    #     x_ = []
+    #     y_ = []
+
+    #     for index, target in df_.iterrows():
+    #         im = cv2.imread(imageDirectory + index)
+    #         im = cv2.resize(cv2.cvtColor(im, cv2.COLOR_BGR2RGB), (IMG_WIDTH, IMG_HEIGHT)).astype(np.float32) / 255.0
+    #         im = np.expand_dims(im, axis =0)
+    #         x_.append(im)
+    #         y_.append(target[attr])
+
+    return x_, y_, df_.index
     
 # x = np.array([load_reshape_img(imageDirectory + fname) for fname in ['000001.jpg','000002.jpg','000003.jpg']])
 # # load_reshape_img(imageDirectory+'000001.jpg')
 # x = x.reshape(x.shape[0], 218, 178, 3)
 # print(x.shape)
-zz = generate_df(parAttr,0, ATTR, TRAINING_SAMPLES)
-print(zz)
+
+
+# zz = generate_df(parAttr,0, ATTR, TRAINING_SAMPLES)
+# print(zz)
+
+# Uncomment from here if you don't have a pretrained model
+#########################################################################
+# inceptionModel = keras.applications.InceptionV3(include_top = False)
+# inceptionModel.trainable = False
+# # print(inceptionModel.summary())
+# x = inceptionModel.output
+# x = layers.GlobalAveragePooling2D()(x)
+# x = layers.Dense(1024, activation="relu")(x)
+# x = layers.Dropout(0.5)(x)
+# x = layers.Dense(512, activation="relu")(x)
+# predictions = layers.Dense(2, activation="softmax")(x)
+# finalModel = keras.Model(inputs = inceptionModel.input, outputs = predictions)
+
+# finalModel.compile(optimizer = 'adam',
+#                 loss=tf.keras.losses.CategoricalCrossentropy(from_logits=False),
+#                 metrics=['categorical_accuracy'])
+
+# xTrain, yTrain, indexes = generate_df(parAttr,0, ATTR, 100000)
+# xTest, yTest, indexes = generate_df(parAttr,2, ATTR, 1000)
+# print(xTrain.shape)
+# print(yTrain.shape)
+# with tf.device('/gpu:0'):
+#     finalModel.fit(xTrain,yTrain,batch_size = 32, epochs = 1)
+#     finalModel.evaluate(xTest,yTest,batch_size = 32)
+# finalModel.save('./projectModelSave')
+#########################################################################
+
+# Load pretrained model
+xTest, yTest, indexes = generate_df(parAttr,1, ATTR, 100)
+newModel = tf.keras.models.load_model('./projectModelSave')
+# newModel.summary()
+# newModel.evaluate(xTest,yTest,verbose = 2)
+
+def randomOneTest():
+    xT, xTruth, dataFrameIndex = generate_df(parAttr,2, ATTR, 1)
+    Prob = newModel.predict(xT)
+    attrResult = 0
+    result = 0
+    if np.argmax(Prob) == 1:
+        attrResult = 1
+    if np.argmax(Prob) == np.argmax(xTruth):
+        result = 1
+    fname = imageDirectory+str(dataFrameIndex[0])
+    image = PIL.Image.open(fname)
+    image.show()
+    if attrResult == 1:
+        print("The subject has the attribute: "+ str(ATTR))
+    else:
+        print("The subject does not have the attribute: "+ str(ATTR))
+    if result == 1:
+        print("The prediction is correct.")
+    else:
+        print("The prediction is NOT correct.")
+    return attrResult, result, fname
+
+randomOneTest()
+
