52abdc1ad3719ea3ce042b3407986e88e0165555
xinranli0809 <xinranli2021@u.northwestern.edu>
2020-12-08 04:05:00 -0500

    performance improvments, and real multi label.

--- celebA.py
+++ celebA.py
@@ -7,7 +7,7 @@ import PIL
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-
+from tensorflow.keras import backend as K
 # comment these off if giving you error
 physicalDevices = tf.config.list_physical_devices('GPU')
 # print(physicalDevices)
@@ -15,7 +15,7 @@ tf.config.experimental.set_memory_growth(physicalDevices[0],True)
 
 ## To use more than one attributes, we will do the same number of 2 class classification over all attributes
 #what characteristic are we going to train and test for? (note if set to 'all' it'll use all of them)
-ATTR='Male','Eyeglasses'
+ATTR='Blond_Hair','Eyeglasses','Male','Smiling','Wearing_Hat'
 
 # set up working directory
 currentWorkingDirectory = os.getcwd()
@@ -52,6 +52,7 @@ def load_reshape_img(fname):
     img = keras.preprocessing.image.load_img(fname)
     # Convert image to np array and normalize
     x = keras.preprocessing.image.img_to_array(img)/255.
+    # x = x.astype(np.uint8)
     # reshape into shape of [1, 218, 178, 3]
     x = x.reshape((1,) + x.shape)
 
@@ -73,27 +74,24 @@ def generate_df(df,partition, attr, num_samples):
     ##  Considering adding back in the future 
 
     #The sample size is at most the number stated above, but at least the size of the smallest class of the dataframe. This results in some uncommon attributes (e.g. sideburns) having to train on very few samples.
-    # min_class_size=min(len(df[(df['partition'] == partition) & (df[attr] == 0)]),len(df[(df['partition'] == partition) & (df[attr] == 1)]) )
-    # sample_size=int(num_samples/2)
-    # if(min_class_size<int(num_samples/2)):
-    #     sample_size=min_class_size
+    min_class_size=min(len(df[(df['partition'] == partition) & (df[attr] == 0)]),len(df[(df['partition'] == partition) & (df[attr] == 1)]) )
+    sample_size=int(num_samples/2)
+    if(min_class_size<int(num_samples/2)):
+        sample_size=min_class_size
     
-    # df_ = df[(df['partition'] == partition) & (df[attr] == 0)].sample(sample_size)
-    # df_ = pd.concat([df_,
-    #                   df[(df['partition'] == partition) 
-    #                               & (df[attr] == 1)].sample(sample_siz)])
+    df_ = df[(df['partition'] == partition) & (df[attr] == 0)].sample(sample_size)
+    df_ = pd.concat([df_, df[(df['partition'] == partition) & (df[attr] == 1)].sample(sample_size)])
 
 
     # sample desired amount of images of input partition
-    df_ = df[(df['partition'] == partition)].sample(num_samples)
+    # df_ = df[(df['partition'] == partition)].sample(num_samples)
     # print(df_)
 
     x_ = np.array([load_reshape_img(imageDirectory + fname) for fname in df_.index])
     # reshape into shape of [# of samples, 218, 178, 3]
-    x_ = x_.reshape(x_.shape[0], 218, 178, 3)
+    x_ = tf.convert_to_tensor(x_.reshape(x_.shape[0], 218, 178, 3))
     # one-hot encode the y values
-    y_ = keras.utils.to_categorical(df_[attr],2)
-
+    y_ = tf.convert_to_tensor(keras.utils.to_categorical(df_[attr],2))
     ## This part is commented out because I don't understand why training and test data are processed in
     ## very different ways yet in the end has the same shape. Use the above method for both train and test for now 
     
@@ -126,112 +124,113 @@ def generate_df(df,partition, attr, num_samples):
 
 # zz = generate_df(parAttr,0, ATTR, TRAINING_SAMPLES)
 # print(zz)
+def loadModel(attribute):
+    model = createModel(0)
+    # expect_partial() is needed to silence warnings about incomplete checkpoint restores
+    # A lot of variables are unused when just predicting, tensorflow prints a warning for each variable you don't use
+    model.load_weights('./models/'+str(attribute)+' Model').expect_partial()
+    return model
+
+def createModel(compileEnable):
+    inceptionModel = keras.applications.InceptionV3(include_top = False)
+    inceptionModel.trainable = False
+    # print(inceptionModel.summary())
+    x = inceptionModel.output
+    x = layers.GlobalAveragePooling2D()(x)
+    x = layers.Dense(1024, activation="relu")(x)
+    x = layers.Dropout(0.5)(x)
+    x = layers.Dense(512, activation="relu")(x)
+    predictions = layers.Dense(2, activation="softmax")(x)
+    finalModel = keras.Model(inputs = inceptionModel.input, outputs = predictions)
+
+    if compileEnable == 1:
+        finalModel.compile(optimizer = 'adam',
+                        loss=tf.keras.losses.CategoricalCrossentropy(from_logits=False),
+                        metrics=['categorical_accuracy'])
+        return finalModel
+    else:
+        return finalModel
+
 
 # Uncomment from here if you don't have a pretrained model
 #########################################################################
-# inceptionModel = keras.applications.InceptionV3(include_top = False)
-# inceptionModel.trainable = False
-# # print(inceptionModel.summary())
-# x = inceptionModel.output
-# x = layers.GlobalAveragePooling2D()(x)
-# x = layers.Dense(1024, activation="relu")(x)
-# x = layers.Dropout(0.5)(x)
-# x = layers.Dense(512, activation="relu")(x)
-# predictions = layers.Dense(2, activation="softmax")(x)
-# finalModel = keras.Model(inputs = inceptionModel.input, outputs = predictions)
-
-# finalModel.compile(optimizer = 'adam',
-#                 loss=tf.keras.losses.CategoricalCrossentropy(from_logits=False),
-#                 metrics=['categorical_accuracy'])
-
-# xTrain, yTrain, indexes = generate_df(parAttr,0, ATTR, 100000)
-# xTest, yTest, indexes = generate_df(parAttr,2, ATTR, 1000)
-# print(xTrain.shape)
-# print(yTrain.shape)
-# with tf.device('/gpu:0'):
-#     finalModel.fit(xTrain,yTrain,batch_size = 32, epochs = 1)
-#     finalModel.evaluate(xTest,yTest,batch_size = 32)
-# finalModel.save('./projectModelSave')
+trainingModel = createModel(1)
+for i in ATTR:
+    print('Training network for : '+i)
+    print('Loading training data')
+    xTrain, yTrain, indexes = generate_df(parAttr,0, i, 160000)
+    print('Loading testing data')
+    xTest, yTest, indexes = generate_df(parAttr,2, i, 19000)
+    print('Begin fitting')
+    with tf.device('/gpu:0'):
+        trainingModel.fit(xTrain,yTrain,batch_size = 32, epochs = 10)
+        print('Result evaluation')
+        trainingModel.evaluate(xTest,yTest,batch_size = 32)
+    trainingModel.save_weights('./models/'+str(i)+' Model')
 #########################################################################
 
-# Load pretrained model
+#########################################################################
+# Turns out it is not great to load all the models first, the best way is to load whatever model you need, and clear session after it.
+# modelList = []
+# # Load pretrained model
+# for i in ATTR:
+#     print('Loading pretrained model for '+ i)
+#     modelList.append(loadModel(i))
+# print('Models loaded')
+#########################################################################
+# @tf.function(experimental_relax_shapes=True)
 
-print('Loading pretrained model, this will take a while (~30 sec to 1 min)')
-# this can be speed up by only saving and loading the weights, maybe
-newModel = tf.keras.models.load_model('./projectModelSave',compile=False)
-print('Model loaded')
-# newModel.summary()
-# xTest, yTest, indexes = generate_df(parAttr,1, ATTR, 100)
-# newModel.evaluate(xTest,yTest,verbose = 2)
-
-
-def testWithTruth(fname):
+def testWithTruth(index,fname):
+    model = loadModel(ATTR[index])
     iname = imageDirectory+str(fname)
     img = PIL.Image.open(iname)
-    img.show()
     img = np.asarray(img)/255.
-    for i in ATTR:
-        groundTruth = keras.utils.to_categorical(attributes.loc[fname,i],2)
-        # print(np.argmax(groundTruth))
-        x = img.reshape((1,) + img.shape)
-        Prob = newModel.predict(x)
-        attrResult = 0
-        result = 0
-        if np.argmax(Prob) == 1:
-            attrResult = 1
-        if np.argmax(Prob) == np.argmax(groundTruth):
-            result = 1
+    groundTruth = keras.utils.to_categorical(attributes.loc[fname,ATTR[index]],2)
+    # print(np.argmax(groundTruth))
+    x = img.reshape((1,) + img.shape)
+    # predict_step() is used now
+    # First, predict() is used to predict a bunch of samples in certain amount of batches.
+    # it also has its own tf.function which was causing retracing warning when used in a loop
+    # not sure what tf.function does yet, but using predict_step() fixed the warning.
+    Prob = model.predict_step(tf.convert_to_tensor(x))
+    attrResult = 0
+    result = 0
+    if np.argmax(Prob) == 1:
+        attrResult = 1
+    if np.argmax(Prob) == np.argmax(groundTruth):
+        result = 1
 
-        if attrResult == 1:
-            print("The subject has the attribute: "+ str(i))
-        else:
-            print("The subject does not have the attribute: "+ str(i))
-        if result == 1:
-            print("The prediction is correct.")
-        else:
-            print("The prediction is NOT correct.")
+    if attrResult == 1:
+        print("The subject has the attribute: "+ str(ATTR[index]))
+    else:
+        print("The subject does not have the attribute: "+ str(ATTR[index]))
+    if result == 1:
+        print("The prediction is correct.")
+    else:
+        print("The prediction is NOT correct.")
     
 
-def test(fname):
+def test(index,fname):
+    model = loadModel(ATTR[index])
     iname = imageDirectory+str(fname)
     img = PIL.Image.open(iname)
-    img.show()
     img = np.asarray(img)/255.
-    for i in ATTR:
-        x = img.reshape((1,) + img.shape)
-        Prob = newModel.predict(x)
-        attrResult = 0
-        result = 0
-        if np.argmax(Prob) == 1:
-            attrResult = 1
-        if attrResult == 1:
-            print("The subject has the attribute: "+ str(i))
-        else:
-            print("The subject does not have the attribute: "+ str(i))
+    x = img.reshape((1,) + img.shape)
+    Prob = model.predict_step(tf.convert_to_tensor(x))
+    attrResult = 0
+    if np.argmax(Prob) == 1:
+        attrResult = 1
+    if attrResult == 1:
+        print("The subject has the attribute: "+ str(ATTR[index]))
+    else:
+        print("The subject does not have the attribute: "+ str(ATTR[index]))
 
-# This function randomly picks images for each attributes you set to evaluate.
-def randomTest():
-    for i in ATTR:
-        xT, xTruth, dataFrameIndex = generate_df(parAttr,2, i, 1)
-        Prob = newModel.predict(xT)
-        attrResult = 0
-        result = 0
-        if np.argmax(Prob) == 1:
-            attrResult = 1
-        if np.argmax(Prob) == np.argmax(xTruth):
-            result = 1
-        fname = imageDirectory+str(dataFrameIndex[0])
-        image = PIL.Image.open(fname)
-        image.show()
-        if attrResult == 1:
-            print("The subject has the attribute: "+ str(i))
-        else:
-            print("The subject does not have the attribute: "+ str(i))
-        if result == 1:
-            print("The prediction is correct.")
-        else:
-            print("The prediction is NOT correct.")
+# Test the example function
+# img = PIL.Image.open(imageDirectory+str('193508.jpg'))
+# img.show()
+# for i in range(len(ATTR)):
+#     testWithTruth(i,'193508.jpg')
+#     # clean up the used model
+#     K.clear_session()
 
-# randomTest()
-# testWithTruth('193508.jpg')
 
